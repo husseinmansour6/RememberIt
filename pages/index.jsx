d3b1e788d44639ee797b3b68a208c2d3f2d2f1db
husseinmansour6 <hmansour@aldogroup.com>
2020-04-10 11:41:23 -0400

    Navigate Between Pages

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+export default function Index() {
+  return (
+    <div>
+      <p>Hello Next.js</p>
+      <Link href="/about">
+        <a title="About Page">About Page</a>
+      </Link>
+    </div>
+  )
+}
